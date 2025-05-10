2d44884d22299e3ba81f98fa3c2e546c9f0fe50f
hirochilove1991 <66413889+hirochilove1991@users.noreply.github.com>
2025-05-10 14:21:25 +0900

    Update config.h

--- qmk_firmware/keyboards/keyball/keyball39/config.h
+++ qmk_firmware/keyboards/keyball/keyball39/config.h
@@ -78,3 +78,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // To squeeze firmware size
 #undef LOCKING_SUPPORT_ENABLE
 #undef LOCKING_RESYNC_ENABLE
+
+// Auto Mouse Layer: Set to Layer 1
+#define AUTO_MOUSE_DEFAULT_LAYER (1UL << 1)
